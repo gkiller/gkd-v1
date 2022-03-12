16525497c401693b4f0a97164bd0018abc1a5ad4
gkiller <blizzm2004@yahoo.om>
2022-03-12 18:22:17 -0500

    Added Feature and Possibility

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,15 +1,52 @@
 import React from 'react';
 import './features.css';
+import { Feature } from '../../components/';
 
+
+
+//  Create array to populate Feature component instead of adding
+//  it four times manually.  Use array map() method iterate through each.
+const featuresData = [
+    
+    {
+        title: 'Improving end distrusts instantly',
+        text: 'Stet diam justo labore voluptua et tempor lorem, justo kasd ut sed et dolor ipsum sanctus dolore sanctus. Dolores dolor.'
+    },
+    
+    {
+        title: 'Become the tended active',
+        text: 'Stet diam justo labore voluptua et tempor lorem, justo kasd ut sed et dolor ipsum sanctus dolore sanctus. Dolores dolor.'
+    },
+
+    {
+        title: 'Message or am nothing',
+        text: 'Stet diam justo labore voluptua et tempor lorem, justo kasd ut sed et dolor ipsum sanctus dolore sanctus. Dolores dolor.'
+    },
+
+    {
+        title: 'Really boy law county',
+        text: 'Stet diam justo labore voluptua et tempor lorem, justo kasd ut sed et dolor ipsum sanctus dolore sanctus. Dolores dolor.'
+    }
+];
 
 //  Functional component
 const Features = () =>{
 
     return(
 
-        //  Parent component
-        <div>
-            Features
+        <div className="gkd__features section__padding" id="features">
+
+            <div className="gkd__features-heading">
+                <h1 className="gradient__text">The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h1>
+                <p>Request Early Access to Get Started</p>
+            </div>
+
+            <div className="gkd__features-container">
+                { featuresData.map( (item, i) => 
+                
+                    <Feature title={item.title} text={item.text} key={item.title + i} />
+                ) }
+            </div>
         </div>
     )
 
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import './possibility.css';
-
+import  possibilityImg  from '../../assets/possibility.png';
 
 //  Functional component
 const Possibility = () =>{
 
     return(
 
-        //  Parent component
-        <div>
-            Possibility
+        <div className="gkd__possibility section__padding" id="possibility">
+
+            <div className="gkd__possibility-image">
+                <img src={ possibilityImg } alt="Possibility" />
+            </div>
+            
+            <div className="gkd__possibility-content">
+                <h4>Request Early Access to Get Started</h4>
+                <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+
+                <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+                <h4>Request Early Access to Get Started</h4>
+            </div>
         </div>
     )
-
 }
 
 
