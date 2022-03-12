afe872994b50f8fa285fcf68088dbbbeedeac214
gkiller <blizzm2004@yahoo.om>
2022-03-11 22:33:19 -0500

    Added WGKD styles and component

--- src/containers/whatGKD/WhatGKD.jsx
+++ src/containers/whatGKD/WhatGKD.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import './whatgkd.css';
-
+import { Feature } from '../../components/';
 
 //  Functional component
 const WhatGKD = () =>{
 
     return(
 
-        //  Parent component
-        <div>
-            WhatGKD
+        <div className="gkd__whatgkd section__margin" id="wgkd">
+            
+            <div className="gkd__whatgkd-feature">
+                <Feature />
+            </div>
+
+            <div className="gkd__whatgkd-heading">
+                <h1 className="gradient__text">Et sea ipsum ut eos ut.</h1>    
+                <p>Explore the library</p>
+            </div>
+
+            <div className="gkd__whatgkd-container">
+                <Feature />    
+                <Feature />    
+                <Feature />    
+            </div>
         </div>
     )
-
 }
 
 
