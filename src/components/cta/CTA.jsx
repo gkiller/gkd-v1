046bd8b0a0b4b147e5909ac876e201fb1919376d
gkiller <blizzm2004@yahoo.com>
2022-03-12 22:22:39 -0500

    Added CTA

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -7,12 +7,17 @@ const CTA = () =>{
 
     return(
 
-        //  Parent component
-        <div>
-            
-        </div>
-    )
+        <div className="gkd__cta">
+            <div className="gkd__cta-content">    
+                <p>Request Early Access to Get Started</p>
+                <h3>Register today & start exploring the endless possiblities.</h3>
+            </div>
 
+            <div className="gkd__cta-button">
+                <button type="button">Get Started</button>
+            </div>
+        </div>  
+    )
 }
 
 
